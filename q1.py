718fd01c372ca8535da3ef833a70aef9ee726cca
GITHUB_USERNAME <KEHILLAH_EMAIL>
2026-04-30 11:44:16 -0700

    all done

--- q1.py
+++ q1.py
@@ -2,6 +2,15 @@ class Cat:
     """a cat"""
     def speak():
         print("Meow!")
+class Dog:
+    """a dog"""
+    def speak():
+        print("Woof!")
+
+millo = Dog()
+lexi = Dog()
+print(millo)
+print(lexi)
 
 ella = Cat()
 zoe = Cat()
